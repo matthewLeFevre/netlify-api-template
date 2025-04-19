4bbbdd9eef089e62234dd4079022abff161c6665
Matthew LeFevre <matthewlefevre95@gmail.com>
2025-04-18 21:50:07 -0600

    👌 IMPROVE: Nasty workarounds for things that should not be issues express.json()?

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,13 +1,24 @@
-import express, { Router } from "express";
+import express, { Router, json } from "express";
 import serverless from "serverless-http";
+import cors from "cors";
+import bodyParseJson from "../middleware/bodyParseJson";
 
 const api = express();
+api.use(cors());
+api.use(json());
+api.use(bodyParseJson);
 
 const router = Router();
 router.get("/", (req, res) => {
   res.json({ message: "Hello from the API!" });
 });
 
+router.post("/", (req, res) => {
+  res.json({ ...req.body });
+});
+
 api.use("/api/", router);
 
-export const handler = serverless(api);
+export const handler = serverless(api, {
+  binary: ["application/json"],
+});
---
+++ netlify/middleware/bodyParseJson.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, RequestHandler, Response } from "express";
+
+const bodyParseJson: RequestHandler = (req, res, next) => {
+  if (req.body instanceof Buffer) {
+    try {
+      req.body = JSON.parse(req.body.toString());
+    } catch (err) {
+      res.status(400).json({ error: "Invalid JSON data" });
+      return;
+    }
+  }
+  next();
+};
+
+export default bodyParseJson;
