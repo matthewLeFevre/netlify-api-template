afc31882a7ce87d4884c031e562ca6b62bfdd42d
Matthew LeFevre <matthewlefevre95@gmail.com>
2025-04-25 19:57:34 -0600

    👌 IMPROVE: testing.

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -1,6 +1,8 @@
 import { Handler } from "@netlify/functions";
+import db from "../db";
 
 export const handler: Handler = async (event, context) => {
+  db;
   return {
     statusCode: 200,
     body: JSON.stringify({
