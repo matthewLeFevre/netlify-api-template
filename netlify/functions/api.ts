cfab79ad79d7e4397b1563f476d77dec1e7fe454
Matthew LeFevre <matthewlefevre95@gmail.com>
2025-05-18 13:59:28 -0600

    👌 IMPROVE: Downgraded express for better compatability with netlify.

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,15 +1,13 @@
 import express, { json } from "express";
 import serverless from "serverless-http";
 import cors from "cors";
-import bodyParseJson from "../middleware/bodyParseJson";
 import exampleRouter from "../router/example";
 import setupDB from "../middleware/setupDb";
 
 const api = express();
 api.use(cors());
-api.use(json());
 api.use(setupDB);
-api.use(bodyParseJson());
+api.use(json());
 
 api.use("/api/", exampleRouter);
 
