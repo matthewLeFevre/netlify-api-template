23a24fceba1c981412bc97088a69a4821a1e2424
Matthew LeFevre <matthewlefevre95@gmail.com>
2025-05-18 15:01:01 -0600

    🐛 FIX: Updates to external modules turso is once again working on netlify functions.

--- netlify/db/schema.ts
+++ netlify/db/schema.ts
@@ -1,7 +1,8 @@
 import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
+import { nanoid } from "nanoid";
 
-export const game = sqliteTable("example", {
-  id: text("id").primaryKey(),
+export const example = sqliteTable("example", {
+  id: text("id").primaryKey().$defaultFn(() => "game-" + nanoid()),
   name: text("name").notNull(),
   count: integer("count").notNull().default(0),
   active: integer("active", { mode: "boolean" }).default(false),
